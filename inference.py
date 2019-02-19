8b0d8a3ff908aab8a0f327c1e1c8d80abd5b3026
Yeongtae <hwak1234@hanmail.net>
2019-02-20 08:53:58 +0900

    save mel

--- inference.py
+++ inference.py
@@ -67,9 +67,11 @@ def mels_to_wavs_GL(hparams, mels, taco_stft, output_dir="", ref_level_db = 0, m
         stime = time.time()
         mel_decompress = mel_denormalize(mel)
         mel_decompress = taco_stft.spectral_de_normalize(mel_decompress + ref_level_db) ** (1/magnitude_power)
-        mel_decompress = mel_decompress.transpose(1, 2).data.cpu()
+        mel_decompress_ = mel_decompress.transpose(1, 2).data.cpu()
+        mel_decompress = mel_decompress.data.cpu().numpy()
+        print(mel_decompress.shape)
         spec_from_mel_scaling = 1000
-        spec_from_mel = torch.mm(mel_decompress[0], taco_stft.mel_basis)
+        spec_from_mel = torch.mm(mel_decompress_[0], taco_stft.mel_basis)
         spec_from_mel = spec_from_mel.transpose(0, 1).unsqueeze(0)
         spec_from_mel = spec_from_mel * spec_from_mel_scaling
         waveform = griffin_lim(torch.autograd.Variable(spec_from_mel[:, :, :]),
@@ -80,6 +82,7 @@ def mels_to_wavs_GL(hparams, mels, taco_stft, output_dir="", ref_level_db = 0, m
         str = "{}th sentence, audio length: {:.2f} sec,  mel_to_wave time: {:.2f}".format(i, len_audio, dec_time)
         print(str)
         write(os.path.join(output_dir,"sentence_{}.wav".format(i)), hparams.sampling_rate, waveform)
+        np.save(os.path.join(output_dir,"mel_{}.wav".format(i)), mel_decompress)
 
 def run(hparams, checkpoint_path, sentence_path, clenaer, silence_mel_padding, output_dir):
     f = open(sentence_path, 'r')
@@ -107,8 +110,8 @@ def run(hparams, checkpoint_path, sentence_path, clenaer, silence_mel_padding, o
 if __name__ == '__main__':
     """
     usage
-    python inference.py -o=synthesis/80000 -c=nam_h_ep8/checkpoint_80000 -s=test.txt --silence_mel_padding=3
-    python inference.py -o=synthesis -c=tacotron2_statedict.pt -s=test.txt --silence_mel_padding=3
+    python inference.py -o=synthesis/80000 -c=nam_h_ep8/checkpoint_80000 -s=test.txt --silence_mel_padding=0
+    python inference.py -o=synthesis -c=tacotron2_statedict.pt -s=test.txt --silence_mel_padding=0
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output_directory', type=str,
